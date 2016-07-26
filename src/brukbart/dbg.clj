976784dedb8df5201976b2c54093cc5137758ef6
Aleksander Madland Stapnes <aleksander_m_stapnes@hotmail.com>
2016-07-25 21:54:39 -0300

    Yak shaving dbg

--- src/brukbart/dbg.clj
+++ src/brukbart/dbg.clj
@@ -2,10 +2,16 @@
   (:require [clojure.pprint :refer [pprint]]))
 
 (defn log-fn
-  "Create a function that applies f (which presumably has side effects) to it's argument,
-  then returns it."
+  "Create a function that applies f (which presumably has side effects)
+  to its argument, then returns it."
   [f]
   (fn [x] (f x) x))
 
-(def p< (log-fn prn))
-(def pp< (log-fn pprint))
+(def p< "Prints a value with prn and returns it."
+  (log-fn prn))
+
+(def pln< "Prints a value with println and returns it."
+  (log-fn println))
+
+(def pp< "Pretty prints a value and returns it."
+  (log-fn pprint))
