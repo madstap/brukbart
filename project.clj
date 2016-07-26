75d754c7b715b5ea2b92aee412d2e02c727296ba
Aleksander Madland Stapnes <aleksander_m_stapnes@hotmail.com>
2016-07-25 21:55:17 -0300

    Update clojure dep to latest alpha to use spec

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject brukbart "0.1.0-SNAPSHOT"
   :description "Some useful stuff"
-  :url "http://example.com/FIXME"
+  :url "http://github.com/madstap/brukbart"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0-alpha10"]
+                 ;; TODO: Use as dev dependency.
+                 [org.clojure/test.check "0.9.0"]])
